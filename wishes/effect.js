f712a93578bd6c96bb448f9ea6081d18efb889c1
Mrudula Didde <shinymrudula@gmail.com>
2018-11-22 23:28:48 +0530

    Countdown timer

--- wishes/effect.js
+++ wishes/effect.js
@@ -196,6 +196,26 @@ $('document').ready(function(){
 });
 
 
+// counter 
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Dec 20, 2018 00:00:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    }, second)
+
+
 
 
 //alert('hello');
