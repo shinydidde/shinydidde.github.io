3a207bbaa6145671af61e9beca729953e339ee6a
Mrudula Didde <shinymrudula@gmail.com>
2018-12-05 22:06:03 +0530

    slowed down the falling hearts

--- personal/js/main.js
+++ personal/js/main.js
@@ -313,10 +313,10 @@
 // Hearts falling
 		var love = setInterval(function() {
 			var r_num = Math.floor(Math.random() * 40) + 1;
-			var r_size = Math.floor(Math.random() * 30) + 10;
+			var r_size = Math.floor(Math.random() * 10) + 10;
 			var r_left = Math.floor(Math.random() * 100) + 1;
 			var r_bg = Math.floor(Math.random() * 25) + 100;
-			var r_time = Math.floor(Math.random() * 5) + 40;
+			var r_time = Math.floor(Math.random() * 5) + 60;
 
 			$('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
 
