76003eb9571f99d6df0f144cbdbd6120e4709df2
Mrudula Didde <shinymrudula@gmail.com>
2018-12-07 03:09:11 +0530

    hearts

--- personal/js/main.js
+++ personal/js/main.js
@@ -317,7 +317,7 @@
 			var r_size = Math.floor(Math.random() * 10) + 10;
 			var r_left = Math.floor(Math.random() * 100) + 1;
 			var r_bg = Math.floor(Math.random() * 25) + 100;
-			var r_time = Math.floor(Math.random() * 5) + 70;
+			var r_time = Math.floor(Math.random() * 5) + 100;
 
 			$('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
 
@@ -330,7 +330,7 @@
 					$(this).detach();
 				}
 			});
-		}, 1000);
+		}, 500);
 		
 		$(".button").click(function toggleMuteAudio(){
 			$(".audioDemo").prop("muted",!$(".audioDemo").prop("muted"));
