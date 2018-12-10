7f5492f8b062f9898a96fd308604627a5c21bb6a
Mrudula Didde <shinymrudula@gmail.com>
2018-12-10 21:55:16 +0530

    Updates

---
+++ precache-manifest.49c842069c0cdef12c62da93a9cb211e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/invitation/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e014fff57d84fb803843",
+    "url": "/invitation/static/js/runtime~main.e014fff5.js"
+  },
+  {
+    "revision": "eaca0b03c7a0f8103631",
+    "url": "/invitation/static/js/main.eaca0b03.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/invitation/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "eaca0b03c7a0f8103631",
+    "url": "/invitation/static/css/main.fce2aea5.chunk.css"
+  },
+  {
+    "revision": "a8f555b01882b50ca6b4dceb67f252ad",
+    "url": "/invitation/index.html"
+  }
+];
